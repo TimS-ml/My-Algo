1aa9defb9fd518ce4237ee05440a3aa8d5915414
jxueba <rfcpdq@gmail.com>
2019-09-08 18:21:36 -0400

    Day 06-09

---
+++ 204-计数质数-改.py
@@ -0,0 +1,31 @@
+# https://leetcode-cn.com/problems/count-primes/
+# 埃拉托色尼筛选法
+
+
+class Solution:
+    def find_primes(self, n):
+        primes = [1] * n  # 0-(n-1)
+        primes[0] = 0
+        primes[1] = 0
+        for i in range(2, int(n**0.5)+1):
+            if primes[i] == 1:  # 加了if就缩短了一半的时间
+                # print('i', i)
+                j = i ** 2
+                if j >= n:  # 加了if就缩短了1/3的时间
+                    return primes
+                while j < n:
+                    # print('j', j)
+                    primes[j] = 0
+                    j += i
+        return primes
+
+    def countPrimes(self, n) -> int:
+        if n <= 2:
+            return 0
+        ans = self.find_primes(n)
+        return sum(ans)
+
+
+n = 16
+print(Solution().find_primes(n))
+print(Solution().countPrimes(n))
---
+++ P-800-1-Similar RGB Color.py
@@ -0,0 +1,6 @@
+# https://leetcode-cn.com/problems/similar-rgb-color/
+
+
+class Solution:
+    def similarRGB(self, color: str) -> str:
+        
