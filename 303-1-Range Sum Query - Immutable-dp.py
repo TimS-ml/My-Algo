1f7be33161d4a193ee07b50ad46b6c27ec9f4e4a
Tim Shen <jianshen@pku.edu.cn>
2019-11-12 17:38:28 -0500

    191112

---
+++ 303-1-Range Sum Query - Immutable-dp.py
@@ -0,0 +1,20 @@
+# https://leetcode-cn.com/problems/range-sum-query-immutable/
+
+
+class NumArray:
+    def __init__(self, nums):
+        
+
+    def sumRange(self, i: int, j: int) -> int:
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(i,j)
+
+nums = [-2, 0, 3, -5, 2, -1]
+# sumRange(0, 2)  # -> 1
+# sumRange(2, 5)  # -> -1
+# sumRange(0, 5)  # -> -3
+print(NumArray().sumRange())
