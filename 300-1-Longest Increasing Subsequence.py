fcd91528bdf7ad3af5692e30030ed1b0ae998a66
Tim Shen <jianshen@pku.edu.cn>
2019-11-12 17:38:28 -0500

    191112

---
+++ 300-1-Longest Increasing Subsequence.py
@@ -0,0 +1,26 @@
+# https://leetcode-cn.com/problems/longest-increasing-subsequence/
+# find the largest end element in tails that is smaller than nums[i]
+# and then replace it with nums[i] and discard the list in the same length
+# which is implemented by 'tail[idx] = i'
+
+
+import bisect
+
+
+class Solution:
+    def lengthOfLIS(self, nums) -> int:
+        tail = []
+        for i in nums:
+            # please remember bisect is for sorted list
+            # tail[0] will be the minium number in the input
+            idx = bisect.bisect_left(tail, i)
+            if idx == len(tail):
+                tail.append(i)
+            else:
+                tail[idx] = i
+            print(idx, tail)
+        return len(tail)
+
+
+nums = [10,9,2,5,3,7,101,18]
+print(Solution().lengthOfLIS(nums))
