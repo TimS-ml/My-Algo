295a69a14ba4c4cd7d642a0be6f6be8cafd38b33
Jian Shen <jianshen@pku.edu.cn>
2019-11-13 01:41:02 -0500

    Some modifies

--- 000_Undo Questions_v2.1.py
+++ 000_Undo Questions_v2.1.py
@@ -1,10 +1,8 @@
-# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.append.html
-# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.count.html
+# update Mark question
 
 
 import os
 import pandas as pd
-# import pprint
 
 
 # open file README.md
@@ -21,38 +19,43 @@ for pyfile in os.listdir(PATH):
 # print(finish)
 
 # buile data frame
-question = pd.DataFrame(columns = ["Day", "Q", "D", "St"])
+question = pd.DataFrame(columns = ["Day", "Q", "Dif", "St"])
 for line in file:
     if 'Day' in line:
         Day = line.split(' ')[2]
         Day = Day.split('\n')[0]
 
-    if 'pending' in line:
+    if ' = ' in line:
         Q = line.split(' ')[0][1:]
         D = line[0]
-        S = 'P'
+        S = line.split(' = ')[1]  # the question should be finish or pending
+        S = S.split('\n')[0]
+
     elif line[0] == 'E' or line[0] == 'M' or line[0] == 'H':
         Q = line.split(' ')[0][1:]
         Q = Q.split('\n')[0]
         D = line[0]
         S = 'Y'
+
     else:
         continue
 
-    if Q not in finish and S != 'P':
+    if Q not in finish and len(S) == 1:
         S = 'N'
 
     question = question.append({
         'Day': Day, 
         'Q': Q, 
-        'D': D,
+        'Dif': D,
         'St': S}, ignore_index=True)
 # print(question.describe())
 
 print(question['St'].value_counts())
 print(question.loc[question.St == 'N'])
-# print(question.loc[question.St == 'P'])
+# print(question[(question.St!="Y")&(question.St!="N")])
 
+
+# output to txt
 # df = question.loc[question.St == 'N']
 # df.to_csv('000_Undo.txt', index=None, sep=' ', mode='a')
 # print('Done!')
