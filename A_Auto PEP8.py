9c430d4b43195d962f7c13cb91e0b41e05f45886
Tim Shen <jianshen@pku.edu.cn>
2020-02-08 13:08:21 -0500

    auto pep8 code now support path with spaces

--- A_Auto PEP8.py
+++ A_Auto PEP8.py
@@ -5,11 +5,11 @@ import subprocess
 PATH = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 os.chdir(PATH)
 
-cmdCommand = "autopep8 " + PATH + \
-    " --recursive --in-place --pep8-passes 2000 --verbose"
-print(PATH, cmdCommand)
+cmd = [r"autopep8", PATH, "--recursive", "--in-place",
+       "--pep8-passes", "2000", "--verbose"]
+print(cmd)
 
-process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
+process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
 output, error = process.communicate()
 
 print('Done!')
