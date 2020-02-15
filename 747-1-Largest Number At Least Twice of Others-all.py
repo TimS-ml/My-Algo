e080dcab57d8898ed4972fee600d62c325bc6faf
Tim Shen <jianshen@pku.edu.cn>
2020-02-15 07:35:31 -0500

    0215

---
+++ 747-1-Largest Number At Least Twice of Others-all.py
@@ -0,0 +1,17 @@
+# https://leetcode-cn.com/problems/largest-number-at-least-twice-of-others/
+# https://www.programiz.com/python-programming/methods/built-in/all
+from typing import List
+
+
+class Solution:
+    def dominantIndex(self, nums: List[int]) -> int:
+        m = max(nums)
+        # print([m >= 2*x for x in nums if x != m])
+        # print([m >= 2*x for x in nums])  # False when nums.index, so will return False in all(xxx) function
+        if all(m >= 2*x for x in nums if x != m):
+            return nums.index(m)
+        return -1
+
+
+nums = [3, 6, 1, 0]
+print(Solution().dominantIndex(nums))
---
+++ 747-2-Largest Number At Least Twice of Others-enumerate.py
@@ -0,0 +1,28 @@
+# https://leetcode-cn.com/problems/largest-number-at-least-twice-of-others/
+from typing import List
+
+
+class Solution:
+    def dominantIndex(self, nums: List[int]) -> int:
+        if len(nums) == 0: return -1
+
+        m = -1
+        m2 = -1
+        mIndex = 0
+        
+        for i,n in enumerate(nums):
+            if n >= m:
+                m2 = m
+                m = n
+                mIndex = i
+            elif n > m2:
+                m2 = n
+
+        if m < m2*2:
+            mIndex = -1
+        
+        return mIndex
+
+
+nums = [3, 6, 1, 0]
+print(Solution().dominantIndex(nums))
