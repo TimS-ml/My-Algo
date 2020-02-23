c1f69c6a3956a23909589fb32d3b02f141a94133
Tim Shen <jianshen@pku.edu.cn>
2020-02-23 10:34:34 -0500

    E028 ans4

--- 028-4-Implement strStr()-Sunday.py
+++ 028-4-Implement strStr()-Sunday.py
@@ -1,6 +1,7 @@
 # https://leetcode-cn.com/problems/implement-strstr/
 # https://leetcode-cn.com/problems/implement-strstr/solution/python3-sundayjie-fa-9996-by-tes/
 # Sunday is easier to understand then KMP
+# The ord() function returns an integer representing the Unicode character
 
 
 class Solution:
@@ -10,19 +11,21 @@ class Solution:
             return []
         right = [-1 for i in range(256)]
         for i, c in enumerate(str):
-            right[ord(c)] =  i
+            right[ord(c)] = i
+            print(ord(c), i)
         return right
 
     def strStr(self, haystack, needle):
         right = self.getRight(needle)
         lenN = len(needle)
         lenH = len(haystack)
-        i = 0
+        i = 0  # start pointer
         while i <= lenH-lenN:
             j = lenN - 1
-            while j >= 0:
+            while j >= 0:  # loop inside needle
                 if haystack[i+j] != needle[j]:
                     if i+lenN < lenH:
+                        # calculate how many steps can skip
                         skip = lenN - right[ord(haystack[i+lenN])]
                     else:
                         return -1
@@ -34,7 +37,6 @@ class Solution:
         return -1
 
 
-haystack = "hello"
-needle = "ll"
-# needle = ""
+haystack = "hello, how's going?"
+needle = "going"
 print(Solution().strStr(haystack, needle))
