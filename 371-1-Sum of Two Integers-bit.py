291e2851135a833fc005f5fd00b3f0b3bbe17e5e
Tim Shen <jianshen@pku.edu.cn>
2020-03-24 13:24:50 -0400

    371

--- 371-1-Sum of Two Integers-bit.py
+++ 371-1-Sum of Two Integers-bit.py
@@ -1,4 +1,5 @@
 # https://leetcode-cn.com/problems/sum-of-two-integers/
+# https://leetcode-cn.com/problems/sum-of-two-integers/solution/wei-yun-suan-xiang-jie-yi-ji-zai-python-zhong-xu-y/
 # https://wiki.python.org/moin/BitwiseOperators
 # https://leetcode.com/problems/sum-of-two-integers/discuss/84278/A-summary%3A-how-to-use-bit-manipulation-to-solve-problems-easily-and-efficiently
 
@@ -23,18 +24,30 @@ https://en.wikipedia.org/wiki/Two%27s_complement
 Returns the complement of x - the number you get by switching each 1 for a 0 and each 0 for a 1. This is the same as -x - 1.
 
 x ^ y exclusive OR
+**the carryless addition**
 https://en.wikipedia.org/wiki/XOR_gate
 Does a "bitwise exclusive or". Each bit of the output is the same as the corresponding bit in x if that bit in y is 0, and it's the complement of the bit in x if that bit in y is 1.
 '''
 
+# Integers are not 32-bit in Python !!!
+
+import pysnooper
+
+
+# @pysnooper.snoop()
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        print(bin(a), bin(b))
         MAX_INT = 0x7FFFFFFF
-        MIN_INT = 0x80000000
-        MASK = 0x100000000
+        MIN_INT = 0x80000000  # MAX_INT + 1
+        MASK = 0x100000000  # 2^32
         while b:
-            a, b = (a ^ b) % MASK, ((a & b) << 1) % MASK
+            print(bin(a), bin(b))
+            # a, b = (a ^ b) % MASK, ((a & b) << 1) % MASK
+            carry = (a & b) << 1
+            a = (a ^ b) % MASK
+            b = carry % MASK
+        # if a is negative, get a's 32 bits complement positive first
+        # then get 32-bit positive's Python complement negative
         return a if a <= MAX_INT else ~((a % MIN_INT) ^ MAX_INT)
 
 
