5e27ca34ff4e78c1c801f628efaef980f1069218
Tim Shen <jianshen@pku.edu.cn>
2020-03-28 21:20:25 -0400

    lol

---
+++ 758-1-Bold Words in String-find.py
@@ -0,0 +1,21 @@
+# https://leetcode-cn.com/problems/bold-words-in-string/
+# See question 616
+
+
+class Solution:
+    def boldWords(self, words, S: str) -> str:
+        mark = [0] * len(S)
+        for d in words:
+            pos = S.find(d)
+            while pos != -1:  # .find return -1 when not find
+                mark[pos:pos+len(d)] = [1] * len(d)
+                pos = S.find(d, pos + 1)  # s.find(value, start, end)
+
+        ans = []
+        for i in range(len(S)):
+            if mark[i] and (i == 0 or not mark[i-1]):
+                ans.append("<b>")
+            ans.append(S[i])
+            if mark[i] and (i == len(S)-1 or not mark[i+1]):
+                ans.append("</b>")
+        return "".join(ans), mark
