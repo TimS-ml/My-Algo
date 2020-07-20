ee37128bfda17c2880f8b61117f0e61df54fe6ec
Tim Shen <jianshen@pku.edu.cn>
2020-07-20 16:58:34 -0400

    Commit on 2020-07-20 16:58

--- 092-1-recursive-two pointers-backtracking-impt1.py
+++ 092-1-recursive-two pointers-backtracking-impt1.py
@@ -1,10 +1,10 @@
 '''
 # Code Explain:
-- Time complexity: O()
-- Space complexity: O()
+- Time complexity: O(n)
+- Space complexity: O(n)
     - The extra space comes from implicit stack space due to recursion, up to n levels deep
 
-What we can do there is to simply have two pointers, one at the beginning of the array and one at the end ('left' and 'right').
+What we can do there is to simply have two pointers, one at the beginning of the array and one at the end ('l' and 'r').
 We repeatedly swap elements pointed to by these two pointers and we move both the pointers towards the center of the array.
 
 However, we don't have any backward pointers in our linked list and neither do we have any indexes. (Question requires do it in one-pass)
@@ -15,14 +15,23 @@ In a recursive, we need:
     [2] a set of rules: recurrence relation
     [3] terminating senario
 
+Steps:
+- Move l and r to m
+- Move r to n
+- Backtracking r + Move l
+    - Swap values
+    - Until r crossed l
+
 # Pros and Cons:
 ## Pros:
+- Only need 2 pointers
+- No special cases
 
 ## Cons:
 - We need to change the value of Linked List
 
 # Notation:
-
+- recurse_and_reverse(r, m-1, n-1) separate the function into two parts
 '''
 
 class ListNode:
@@ -33,8 +42,35 @@ class ListNode:
 
 class Solution:
     def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
-        if not head or not head.next:
-            return head
+        if not head:
+            return None
+
+        l, r = head, head
+        stop = False
+        
+        def recurse_and_reverse(r, m, n):
+            nonlocal l, stop
+            
+            # Part 1
+            # base case
+            if n == 1:
+                return
+            
+            # move to the proper node
+            r = r.next
+            if m > 1:
+                l = l.next
+
+            recurse_and_reverse(r, m-1, n-1)
+
+            # Part 2
+            if l == r of l = r.next:
+                stop = True
+
+            if not stop:
+                l.val, r.val = r.val, l.val
+                # r moves one step back via backtracking
+                l = l.next  # pay attention on that
 
 
 def listToListNode(input):
