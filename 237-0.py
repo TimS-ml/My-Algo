42b0aa7462a83dc468d409f40fb984c2a3b2aba3
Tim Shen <jianshen@pku.edu.cn>
2020-07-21 01:16:54 -0400

    Commit on 2020-07-21 01:16

---
+++ 237-0.py
@@ -0,0 +1,54 @@
+'''
+# Code Explain:
+- Time complexity: O(1)
+- Space complexity: O(1)
+
+Treat this as a overwrite
+
+# Pros and Cons:
+## Pros:
+
+## Cons:
+
+# Notation:
+
+'''
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def deleteNode(self, node):
+        node.val = node.next.val
+        node.next = node.next.next
+
+
+def listToListNode(input):
+    # Now convert that list into linked list
+    dummyRoot = ListNode(0)
+    ptr = dummyRoot
+    for number in input:
+        ptr.next = ListNode(number)
+        ptr = ptr.next
+    ptr = dummyRoot.next
+    return ptr
+
+
+def listNodeToString(node):
+    if not node:
+        return "[]"
+    result = ""
+    while node:
+        result += str(node.val) + ", "
+        node = node.next
+    return "[" + result[:-2] + "]"
+
+
+line = [1, 2, 3, 4, 5]
+head = listToListNode(line)
+ans = Solution().xxx(head)
+out = listNodeToString(ans)
+print(out)
---
+++ 237-Delete Node in a Linked List-linkedlist.md
@@ -0,0 +1,9 @@
+# Links
+https://leetcode-cn.com/problems/delete-node-in-a-linked-list/
+https://leetcode.com/problems/delete-node-in-a-linked-list/discuss/
+https://leetcode.com/problems/delete-node-in-a-linked-list/solution/
+
+# Thought Process
+
+# Test Cases
+
