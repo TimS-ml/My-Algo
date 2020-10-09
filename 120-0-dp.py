04fcb121c0a85c42097a7be5c3a43a1d71ff8d26
Tim Shen <jianshen@pku.edu.cn>
2020-10-09 09:20:13 -0400

    Commit on 2020-10-09 09:20

--- 120-0-dp.py
+++ 120-0-dp.py
@@ -4,11 +4,12 @@
 - Space complexity: O(n^2)
 
 [1] Base State
-- triangle[i][j]: optimal path to row i col j
+- dp[i][j]: optimal path to row i col j
 [2] State Transfer Equation
-- triangle[i][j] = min(triangle[i+1][j], triangle[i+1][j+1]) + t[i][j]
+- dp[i][j] = min(dp[i+1][j], dp[i+1][j+1]) + t[i][j]
 [3] Initialize Conditions
-- triangle[n][j] = t[n][j]
+- dp[n-1][j] = t[n-1][j]
+- or outlier == 0
 [4] State Compression (optional)
 [5] Terminate Conditions
 
@@ -23,28 +24,24 @@
 
 from typing import List
 
+
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        for i in reversed(range(len(triangle)-1)):
-            for j in range(i+1):
-                triangle[i][j] = min(triangle[i+1][j], triangle[i+1][j+1]) + triangle[i][j]
+        for i in reversed(range(len(triangle) - 1)):
+            for j in range(i + 1):
+                triangle[i][j] = min(triangle[i + 1][j],
+                                     triangle[i + 1][j + 1]) + triangle[i][j]
         return triangle[0][0]
 
     # Space complexity: O(n)
     def minimumTotal2(self, triangle: List[List[int]]) -> int:
-        dp = [0 for i in range(len(triangle)+1)]
+        dp = [0 for i in range(len(triangle) + 1)]
         for i in reversed(range(len(triangle))):
-            for j in range(i+1):
-                dp[j] = min(dp[j], dp[j+1]) + triangle[i][j]
+            for j in range(i + 1):
+                dp[j] = min(dp[j], dp[j + 1]) + triangle[i][j]
         print(dp)
         return dp[0]
 
 
-t = [
-    [2],
-    [3,4],
-    [6,5,7],
-    [4,1,8,3]
-]
+t = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]
 print(Solution().minimumTotal2(t))
-
--- 120-1-dfs.py
+++ 120-1-dfs.py
@@ -4,11 +4,12 @@
 - Space complexity: O(n^2)
 
 [1] Base State
-- triangle[i][j]: optimal path to row i col j
+- dp[i][j]: optimal path to row i col j
 [2] State Transfer Equation
-- triangle[i][j] = min(triangle[i+1][j], triangle[i+1][j+1]) + t[i][j]
+- dp[i][j] = min(dp[i+1][j], dp[i+1][j+1]) + t[i][j]
 [3] Initialize Conditions
-- triangle[n][j] = t[n][j]
+- dp[n-1][j] = t[n-1][j]
+- or outlier == 0
 [4] State Compression (optional)
 [5] Terminate Conditions
 
@@ -18,23 +19,37 @@
 ## Cons:
 
 # Notation:
-
+the difference between dp and dfs is:
+dfs: top-down 
 '''
 
 from typing import List
 
+
 class Solution:
     # basic recursion
     def minimumTotal(self, triangle: List[List[int]]) -> int:
+        def dfs(i, j):
+            if i == len(triangle):
+                return 0
+            return min(dfs(i + 1, j), dfs(i + 1, j + 1)) + triangle[i][j]
+
+        return dfs(0, 0)
 
     def minimumTotal2(self, triangle: List[List[int]]) -> int:
+        cache = [[None for i in range(len(triangle))]
+                 for j in range(len(triangle))]
+
+        def dfs(i, j):
+            if i == len(triangle):
+                return 0
+            if not cache[i][j]:
+                cache[i][j] = min(dfs(i + 1, j), dfs(i + 1,
+                                                     j + 1)) + triangle[i][j]
+            return cache[i][j]
+
+        return dfs(0, 0)
 
 
-t = [
-    [2],
-    [3,4],
-    [6,5,7],
-    [4,1,8,3]
-]
+t = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]
 print(Solution().minimumTotal2(t))
-
