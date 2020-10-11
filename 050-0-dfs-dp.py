7ca523c8e8f5b3f72e5718dab8ba3074d1ff2385
Tim Shen <jianshen@pku.edu.cn>
2020-10-10 23:49:12 -0400

    Commit on 2020-10-10 23:49

--- 050-0-dfs-dp.py
+++ 050-0-dfs-dp.py
@@ -14,6 +14,8 @@ n and x can be both positive and negative
 
 
 class Solution:
+    # dfs
+    # top down
     def myPow(self, x: float, n: int) -> float:
         cache = {}
 
@@ -23,13 +25,19 @@ class Solution:
             if i <= 2:
                 return x**i
             else:
-                ans = Pow(x, int(i / 2)) * Pow(x, i - int(i / 2))
-            cache[i] = ans
-
-            return ans
+                cache[i] = Pow(x, int(i / 2)) * Pow(x, i - int(i / 2))
+            return cache[i]
 
         return Pow(x, n)
 
+    # dp
+    # bottom up
+    def myPow2(self, x: float, n: int) -> float:
+        dp = [1] + [x] + [None] * (n -1)
+        for i in range(1, n+1):
+            dp[i] = dp[int(i/2)] * dp[i - int(i/2)]
+        return dp[n]
+
 
 x, n = 2, 10
-print(Solution().myPow(x, n))
+print(Solution().myPow2(x, n))
