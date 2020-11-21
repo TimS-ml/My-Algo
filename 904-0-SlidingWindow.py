d18ba50e083c36d7150ef59a0b3515f89b368faf
Tim Shen <jianshen@pku.edu.cn>
2020-11-21 01:25:35 -0500

    Commit on 2020-11-21 01:25

---
+++ 904-0-SlidingWindow.py
@@ -0,0 +1,63 @@
+'''
+# Code Explain:
+- Time complexity: O()
+- Space complexity: O()
+
+# Pros and Cons:
+## Pros:
+
+## Cons:
+
+# Notation:
+
+'''
+
+import collections
+from typing import List
+
+class Solution:
+    def totalFruit(self, tree: List[int]) -> int:
+        blocks = [(k, len(list(v)))
+                  for k, v in itertools.groupby(tree)]
+
+        ans = i = 0
+        while i < len(blocks):
+            # We'll start our scan at block[i].
+            # types : the different values of tree[i] seen
+            # weight : the total number of trees represented
+            #          by blocks under consideration
+            types, weight = set(), 0
+
+            # For each block from i and going forward,
+            for j in xrange(i, len(blocks)):
+                # Add each block to consideration
+                types.add(blocks[j][0])
+                weight += blocks[j][1]
+
+                # If we have 3 types, this is not a legal subarray
+                if len(types) >= 3:
+                    i = j-1
+                    break
+
+                ans = max(ans, weight)
+
+            # If we go to the last block, then stop
+            else:
+                break
+
+        return ans
+
+
+    def totalFruit2(self, tree: List[int]) -> int:
+        ans = i = 0
+        count = collections.Counter()
+        for j, x in enumerate(tree):
+            count[x] += 1
+            while len(count) >= 3:
+                count[tree[i]] -= 1
+                if count[tree[i]] == 0:
+                    del count[tree[i]]
+                i += 1
+            ans = max(ans, j - i + 1)
+        return ans
+
---
+++ 904-Fruit Into Baskets.md
@@ -0,0 +1,7 @@
+# Links
+https://leetcode-cn.com/problems/fruit-into-baskets/
+
+# Thought Process
+
+# Test Cases
+
