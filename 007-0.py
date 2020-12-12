f3bc7407a8c03b1bcd6ad187bb794a3a801e0e60
Tim <jianshen@pku.edu.cn>
2020-12-11 20:38:46 -0500

    Commit on 2020-12-11 20:38

--- 007-0.py
+++ 007-0.py
@@ -29,6 +29,20 @@ class Solution:
             ans = -int(str(x)[-1:0:-1])
         return ans if -2**31 < ans < 2**31 - 1 else 0
 
+    def reverse2(self, x):
+        # sign = cmp(x, 0)  # only in python 2.x
+        sign = -1 if x < 0 else 1
+
+        x = abs(x)
+        ans = 0
+
+        while x != 0:
+            # print(x, ans)
+            ans = ans * 10 + x % 10
+            x //= 10
+
+        return ans * sign if -2**31 < ans * sign < 2**31 - 1 else 0
+
 
 # nums, target
 IN = [(123), (-120)]
