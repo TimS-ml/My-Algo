e8531c6eff1e620e929756e471ce12816dabf54d
Jian Shen <jianshen@pku.edu.cn>
2021-01-30 17:53:13 -0500

    Commit on windows

--- 1721-1.py
+++ 1721-1.py
@@ -12,22 +12,26 @@
 
 '''
 
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
+
 class Solution:
     def swapNodes(self, head: ListNode, k: int) -> ListNode:
         curr = head
-        i, j = head, head # 2 swap points
+        i, j = head, head  # 2 swap points
         n = 1
-        while curr:
+        while curr:  # repeat length times
             if n < k:
-                i = i.next  # left
+                # l, i.next: k-1 times => kth element
+                i = i.next
             if n > k:
-                j = j.next  # right, notice that j start at head
-            curr = curr.next
+                # r, j.next: length-k times => length-k+1 th element
+                j = j.next
+                curr = curr.next
             n += 1
         i.val, j.val = j.val, i.val
         return head
