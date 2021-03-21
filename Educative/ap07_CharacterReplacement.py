6b8ede7b46e0fac76405f7c6ec4ec633ee9142ca
TimS <jianshen@pku.edu.cn>
2021-03-20 22:25:47 -0400

    Commit on 2021-03-20 22:25

--- Educative/ap07_CharacterReplacement.py
+++ Educative/ap07_CharacterReplacement.py
@@ -1,15 +1,27 @@
+# time: O(N)
+# space: O(1)
+# same as ap04
+# ap04, ap05 dic is frequency dict
+
+# key point:
+# keep track of the count of the __maximum repeating letter__ in any window (let’s call it maxRepeatLetterCount).
+# - we can have a window which has one letter repeating maxRepeatLetterCount times
+# - this means we should try to replace the remaining letters.
+# - If we have more than ‘k’ remaining letters, we should shrink the window as we are not allowed to replace more than ‘k’ letters
+
+
 def length_of_longest_substring(str1, k):
     window_start, max_length, max_repeat_letter_count = 0, 0, 0
-    frequency_map = {}
+    dic = {}  # frequency_map
 
     # Try to extend the range [window_start, window_end]
     for window_end in range(len(str1)):
         right_char = str1[window_end]
-        if right_char not in frequency_map:
-            frequency_map[right_char] = 0
-        frequency_map[right_char] += 1
+        if right_char not in dic:
+            dic[right_char] = 0
+        dic[right_char] += 1
         max_repeat_letter_count = max(max_repeat_letter_count,
-                                      frequency_map[right_char])
+                                      dic[right_char])
 
         # Current window size is from window_start to window_end, overall we have a letter which is
         # repeating 'max_repeat_letter_count' times, this means we can have a window which has one letter
@@ -18,7 +30,7 @@ def length_of_longest_substring(str1, k):
         # are not allowed to replace more than 'k' letters
         if (window_end - window_start + 1 - max_repeat_letter_count) > k:
             left_char = str1[window_start]
-            frequency_map[left_char] -= 1
+            dic[left_char] -= 1
             window_start += 1
 
         max_length = max(max_length, window_end - window_start + 1)
