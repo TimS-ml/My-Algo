af31cec97a8a3430d70cdd0ab8443952887b0f14
TimS <jianshen@pku.edu.cn>
2021-05-31 09:14:18 -0400

    Commit on 2021-05-31 09:14

--- Educative/bp04_TripletSumToZero.py
+++ Educative/bp04_TripletSumToZero.py
@@ -1,37 +1,48 @@
-# time: O(n^2)
-# Sorting the array will take O(n * logn)
-# The searchPair() function will take O(n)
-#   As we are calling searchPair() for every number in the input array,
-#     this means that overall searchTriplets() will take O(n * logn + n^2),
-#     which is asymptotically equivalent to O(n^2)
-# space: O(n)
+'''
+Given an array of unsorted numbers, find all unique triplets in it that add up to zero
 
-# element may be used multiple times
-# same as bp01
-# sorted first, 2 pointers from left and right move to middle
-# !! skip same element to avoid duplicate triplets
-# what if: [-1, -1, -1, 2] ? add [-1, -1, 2] at arr[0]
+# Code Explain:
+- Time complexity: O(n^2)
+Sorting the array will take O(n * logn)
+The searchPair() function will take O(n)
+  As we are calling searchPair() for every number in the input array,
+    this means that overall searchTriplets() will take O(n * logn + n^2),
+    which is asymptotically equivalent to O(n^2)
+- Space complexity: O(n)
 
-# [-3, -2, -1, 0, 1, 1, 2] 
-# if arr[i] is -3 (x=-3)
-# find y+z = 0-(-3)
+# Pros and Cons and Notation:
+
+same as bp01
+element may be used multiple times
+- sorted first, 2 pointers from left and right move to middle
+- !! skip same element to avoid duplicate triplets
+
+case: [-1, -1, -1, 2] 
+    add [-1, -1, 2] at arr[0]
+case: [-3, -2, -1, 0, 1, 1, 2] 
+    if arr[i] is -3 (x=-3)
+    find y+z = 0-(-3)
+'''
+
 
 def search_triplets(arr):
     arr.sort()
-    triplets = []
+    ans = []
     for i in range(len(arr) - 2):  # -2 is not necessary but good to have
         # skip same element to avoid duplicate triplets
         if i > 0 and arr[i] == arr[i - 1]:
             continue
-        search_pair(arr, -arr[i], i + 1, triplets)
-    return triplets
+        search_pair(arr, -arr[i], i + 1, ans)  # search from i+1 to len-1
+    return ans
 
-
+# 2 pointers from left and right move to middle
 def search_pair(arr, target_sum, left, triplets):
     right = len(arr) - 1
     while (left < right):
         current_sum = arr[left] + arr[right]
         if current_sum == target_sum:  # found the triplet
+            # target_sum is arr[i], left starts at i+1
+            # so the triplets is ascending order
             triplets.append([-target_sum, arr[left], arr[right]])
             # avoid duplicate triplets
             left += 1
--- Educative/bp05_TripletSumCloseToTarget.py
+++ Educative/bp05_TripletSumCloseToTarget.py
@@ -1,19 +1,23 @@
-# time: O(n^2)
-# space: O(n)
+'''
+# Code Explain:
+- Time complexity: O(n^2)
+- Space complexity: O(n)
 
-# same as bp04
-# target_sum is given
-# we need to define a similar var: target_diff
-# in bp04, we need to skip dulicate elements
-# case: [-1, -1, -1, 2], 1
-# since we care about 'most' xxx, thus duplicate check is optional (but nice to have)
+# Pros and Cons and Notation:
+
+same as bp04
+- target_sum is given
+- we need to define a variable to describe `difference`: target_diff
+- in bp04, we need to skip dulicate elements
+'''
+
 
 import math
 
 
 def triplet_sum_close_to_target(arr, target_sum):
     arr.sort()
-    smallest_difference = math.inf
+    smallest_diff = math.inf
     for i in range(len(arr) - 2):
         left = i + 1
         right = len(arr) - 1
@@ -21,58 +25,62 @@ def triplet_sum_close_to_target(arr, target_sum):
             target_diff = target_sum - arr[i] - arr[left] - arr[right]
             if target_diff == 0:  # we've found a triplet with an exact sum
                 return target_sum  # return sum of all the numbers
-
-            # the second part of the following 'if' is to handle the smallest sum when we have more than one solution
-            if abs(target_diff) < abs(smallest_difference) or (
-                    abs(target_diff) == abs(smallest_difference)
-                    and target_diff > smallest_difference):
-                smallest_difference = target_diff  # save the closest and the biggest difference
+            
+            # remember that our goal is to find a smallest_diff(global) close to 0
+            # the second part of the following 'if' is to 
+            #   handle the smallest sum when we have more than one solution
+            #   if simply using target_diff == -smallest_diff will include -1 +1 (t<s case)
+            if abs(target_diff) < abs(smallest_diff) or \
+                    (abs(target_diff) == abs(smallest_diff)
+                        and target_diff > smallest_diff):
+                smallest_diff = target_diff
 
             if target_diff > 0:
                 left += 1  # we need a triplet with a bigger sum
             else:
                 right -= 1  # we need a triplet with a smaller sum
 
-    return target_sum - smallest_difference
+    return target_sum - smallest_diff
 
 
 def triplet_sum_close_to_target_v2(arr, target_sum):
     arr.sort()
-    smallest_difference = math.inf
+    smallest_diff = math.inf
     for i in range(len(arr) - 2):
         if i > 0 and arr[i] == arr[i - 1]:
             continue
         # left = i + 1
         # in bp04, target_sum is 0
-        smallest_difference = search_pair(arr, target_sum-arr[i], i + 1, smallest_difference)
-        if smallest_difference == 0:
+        smallest_diff = search_pair(arr, target_sum-arr[i], i + 1, smallest_diff)
+        if smallest_diff == 0:
             return target_sum
-    return target_sum - smallest_difference
+    return target_sum - smallest_diff
 
 
-def search_pair(arr, target_sum, left, smallest_difference):
+def search_pair(arr, target_sum, left, smallest_diff):
     right = len(arr) - 1
     while (left < right):
         target_diff = target_sum - arr[left] - arr[right]
         if target_diff == 0:  # we've found a triplet with an exact sum
-            return 0  # return smallest_difference
+            return 0  # return smallest_diff
 
-        # the second part of the following 'if' is to handle the smallest sum when we have more than one solution
-        if abs(target_diff) < abs(smallest_difference) or (
-                abs(target_diff) == abs(smallest_difference)
-                and target_diff > smallest_difference):
-            smallest_difference = target_diff  # save the closest and the biggest difference
+        # the second part of the following 'if' is to 
+        #   handle the smallest sum when we have more than one solution
+        if abs(target_diff) < abs(smallest_diff) or \
+                (abs(target_diff) == abs(smallest_diff)
+                    and target_diff > smallest_diff):
+            smallest_diff = target_diff
 
         if target_diff > 0:
             left += 1  # we need a triplet with a bigger sum
         else:
             right -= 1  # we need a triplet with a smaller sum
-    return smallest_difference
+    return smallest_diff
 
 
 def main():
-    # print(triplet_sum_close_to_target([-2, 0, 1, 2], 2))
-    # print(triplet_sum_close_to_target_v2([-2, 0, 1, 2], 2))
+    print(triplet_sum_close_to_target([-2, 0, 1, 2], 2))
+    print(triplet_sum_close_to_target_v2([-2, 0, 1, 2], 2))
     print(triplet_sum_close_to_target([-3, -1, 1, 2], 1))
     print(triplet_sum_close_to_target_v2([-3, -1, 1, 2], 1))
     # print(triplet_sum_close_to_target([1, 0, 1, 1], 100))
