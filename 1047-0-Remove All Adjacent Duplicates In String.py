f05b459873dfd49ec90b17bce61b2649d3df5dbc
TimS <jianshen@pku.edu.cn>
2021-07-01 00:35:01 -0400

    Commit on 2021-07-01 00:35

--- 1047-0-Remove All Adjacent Duplicates In String.py
+++ 1047-0-Remove All Adjacent Duplicates In String.py
@@ -1,19 +1,36 @@
 '''
 # Code Explain:
-- Time complexity: O()
-- Space complexity: O()
+- Time complexity: O(n)
+- Space complexity: O(n)  # dose python provide in/out stack?
 
 # Pros and Cons and Notation:
 
-- How to remove duplicate chars?
-    - copy of string or pop
-- Recursion?
-
 '''
+
 
 class Solution:
     def removeDuplicates(self, s: str) -> str:
-        for i in range(len(s)-1):
-            if s[i] == s[i+1]:
-                # do sth
-                continue
+        ans = list()
+        for c in s:
+            if ans and ans[-1] == c:
+                ans.pop()
+            else:
+                ans.append(c)
+        return "".join(ans)
+
+    def removeDuplicates_2(self, s: str) -> str:
+        def recursion(arr):
+            if len(arr) < 2:
+                return arr
+
+            for i in range(len(arr) - 1):
+                if arr[i] == arr[i+1]:
+                    arr = arr.replace(arr[i]+arr[i], '')
+                    return recursion(arr)
+            return arr
+        return recursion(s)
+
+
+s = 'abbaca'
+print(Solution().removeDuplicates(s))
+print(Solution().removeDuplicates_2(s))
