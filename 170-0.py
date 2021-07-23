fd5d1a88b5c87cb3c9fa11faa11a14e7f36840bb
TimS <jianshen@pku.edu.cn>
2021-07-23 17:18:02 -0400

    Commit on 2021-07-23 17:18

---
+++ 170-0.py
@@ -0,0 +1,38 @@
+'''
+# Code Explain:
+- Time complexity: O(N)
+- Space complexity: O(1)
+
+# Pros and Cons and Notation:
+
+ord('A')=65
+
+AB  =  1*26 + 2
+XY  = 24*26 + 25
+ZZ  = 26*26
+AAB = 1*26**2 + AB = 704
+'''
+
+
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        col_li = list(columnTitle)[::-1]
+        ans = 0
+        for i in range(len(col_li)):
+            dig = ord(col_li[i]) - ord('A') + 1
+            ans += dig * 26**i
+        return ans
+
+    def titleToNumber_2(self, columnTitle: str) -> int:
+        ans = 0
+        # order of i is: 2, 1, 0
+        power = 0
+        for i in range(len(columnTitle) - 1, -1, -1):
+            dig = ord(columnTitle[i]) - ord('A') + 1
+            ans += dig * power
+            power *= 26
+        return ans
+
+
+columnTitle = 'AAB'
+print(Solution().titleToNumber_2(columnTitle))
--- 977-0.py
+++ 977-0.py
@@ -1,9 +1,49 @@
 '''
 # Code Explain:
-- Time complexity: O()
-- Space complexity: O()
+sol 1
+- Time complexity: O(NlongN)
+- Space complexity: O(longN)
 
-# Pros and Cons and Notation:
+sol 2
+- Time complexity: O(N)
+- Space complexity: O(1)
 
+- find boundary of <0 and >=0
+- two pointers from middle to edge
 '''
 
+from typing import List
+
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        nums = [n**2 for n in nums]
+        return sorted(nums)
+
+    def sortedSquares_2(self, nums: List[int]) -> List[int]:
+        L = len(nums)
+        neg = -1  # init negative value location
+        for k, v in enumerate(nums):
+            if v < 0:
+                neg = k
+            else:
+                break
+
+        ans = []
+        l, r = neg, neg + 1
+
+        while l >= 0 or r < L:
+            if l < 0:  # move r
+                ans.append(nums[r]**2)
+                r += 1
+            elif r == L:  # move l
+                ans.append(nums[l]**2)
+                l -= 1
+            else:
+                if nums[l]**2 < nums[r]**2:
+                    ans.append(nums[l]**2)
+                    l -= 1
+                else:
+                    ans.append(nums[r]**2)
+                    r += 1
+        return ans
