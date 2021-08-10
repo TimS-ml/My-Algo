b680d49f9bc198e0056100a4e5dc45f5e779c268
TimS <jianshen@pku.edu.cn>
2021-08-10 11:54:16 -0400

    Commit on 2021-08-10 11:54

---
+++ 033-0-binary_search.py
@@ -0,0 +1,56 @@
+'''
+# Code Explain:
+- Time complexity: O(logn)
+- Space complexity: O(1)
+
+# Pros and Cons and Notation:
+- The array itself is not ordered. After the rotation, only the parts of the array are guaranteed to be ordered
+    - All are ascending arrays except for a sudden drop at a certain point
+
+case: [7, 0, 1, 2] mid is 1, target is 7
+we cannot use target vs mid to locate array
+so
+[1] make sure [left, mid] or [mid, right] is ascending order array
+[2] check if tartget inside that ascending array
+
+    nums[0] <= target <= nums[mid]
+               target <= nums[mid] < nums[0]
+                         nums[mid] < nums[0] <= target
+
+what we need to compare
+(nums[0] <= target)， (target <= nums[mid]) ，(nums[mid] < nums[0])
+
+- If [left, mid-1] is an ascending ordered array, and 'target' in [nums[left], nums[mid])
+    - then we should narrow the search scope to [left, mid-1], otherwise, search in [mid+1, right]
+- If [mid, right] is an ascending ordered array, and 'target' in (nums[mid+1], nums[right]]
+    - then we should narrow the search range to [mid+1, right], otherwise we should search in [left, mid-1].
+'''
+
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+
+        # init 2 pointers
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid  # this is the only place to return answer
+            
+            # update l and r
+            if nums[0] <= nums[mid]:  # ascending order
+                if nums[0] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target <= nums[len(nums) - 1]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
