30dc13e01524631c535b3ab3b665b030f8e12313
TimS <jianshen@pku.edu.cn>
2021-08-15 19:51:29 -0400

    Commit on 2021-08-15 19:51

--- 413-0-dp.py
+++ 413-0-dp.py
@@ -1,17 +1,26 @@
 '''
 - kws: consecutive
 - brute force or dp
+- arithmetic slices: 
+    max len = n
+    nums(len window = n) + nums(len window = n-1) ... nums(len window = 3)
+    1 + 2 + 3 ... n-2
+    (n-2+1) * (n-2)/2 = (n-1)(n-2)/2
+
+- [1] get the number and length of arithmetic subarrays block
+- [2] result = ...
 
 dp[i]:
 number of arithmetic subarrays of nums[:i]
 
 state transfer:
-dp[i] = dp[i-1]
+if difference between any two consecutive elements is the same:
+    dp[i] = dp[i-1] + (n-2)
+    we need to know where the seq start
 
 init:
 dp[0~1] = 0
 '''
-
 
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
