5932082eed516fe0982a5c72065b79e62840d3bf
TimS <jianshen@pku.edu.cn>
2022-02-13 16:21:51 -0800

    lc 680 with unit test

--- LeetCode/680-Valid Palindrome II.md
+++ LeetCode/680-Valid Palindrome II.md
@@ -1,8 +1,13 @@
 # Links
 https://leetcode-cn.com/problems/valid-palindrome-ii/
+
+with N del
 https://leetcode-cn.com/problems/valid-palindrome-ii/solution/ji-yu-yan-ge-yan-zheng-hui-wen-chuan-he-fa-xing-de/
+https://leetcode.com/problems/valid-palindrome-ii/discuss/1764631/Simple-Generalized-Javascript-solution-for-N-deletes
+
 
 # Thought Process
+
 
 # Test Cases
 
---
+++ LeetCode/680-m-0.py
@@ -0,0 +1,75 @@
+'''
+# Code Explain:
+- Time complexity: O(n)
+- Space complexity: O(1)
+
+# Thoughts:
+s is palindrome:
+- s == s[::-1]
+- if s2 = s[x:-x], x <= len(s) // 2
+    s2 is palindrom
+- When l <= r:
+    s[l] == s[r]
+    l += 1
+    r -= 1
+
+del at most one:
+- When s[l] != s[r]:
+    counter += 1  # should I move l or r ???
+
+
+two pointers:
+move to middle
+s = "abca"
+     |  |
+     l  r
+
+# Solution:
+move l and r to middle
+if s[l] != s[r]
+    sl = s[l+1:r]
+    sr = s[l:r-1]
+    find if sl or sr is palindrom
+
+
+# Follow up:
+del at most n:
+- counter = 0
+- counter <= 1
+- tree structure
+'''
+
+import pytest
+
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s)-1
+
+        while l < r:
+            if s[l] == s[r]:
+                l += 1 
+                r -= 1
+            else:
+                sl = s[l+1:r+1]
+                sr = s[l:r]
+                # print(sl, sr)
+                return sl == sl[::-1] or \
+                        sr == sr[::-1]
+
+        return True
+
+
+@pytest.mark.parametrize(
+    "test_input, expected", 
+    [("", True),
+     ("a", True),
+     ("abobca", True),
+     ("aaa", True),
+     ("abc", False),
+     ("abab", True)]
+)
+
+def test_func(test_input, expected):
+    assert Solution().validPalindrome(test_input) == expected
+
+# pytest.main()
