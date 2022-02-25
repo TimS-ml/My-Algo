e3969d3254af8187587e1db6c4cceddb0dc9d53f
TimS <jianshen@pku.edu.cn>
2022-02-24 20:08:01 -0800

    1762

--- LeetCode/1762-m-0.py
+++ LeetCode/1762-m-0.py
@@ -19,7 +19,39 @@ compare if li[i] > tallest_li(i)
 # Stack
 Know ans[-1] = 1
 
+# Loop reverslly
 Loop reverslly, we should ideally found an increasing seq
 Just mark the non increasing seq idx
 '''
 
+from typing import List
+
+class Solution:
+    def findBuildings(self, heights: List[int]) -> List[int]:
+        n = len(heights)
+        answer = []
+        max_height = -1
+        
+        for current in reversed(range(n)):
+            if heights[current] > max_height:
+                answer.append(current)
+                max_height = heights[current]
+        
+        answer.reverse()  # remember to reverse it
+        return answer
+
+    def findBuildings_2(self, heights: List[int]) -> List[int]:
+        '''
+        The same as v1 but using stack and not looping reverselly
+        '''
+        n = len(heights)
+        answer = []
+        stack = []
+        
+        for current in range(n):
+            while stack and heights[current] > stack[-1]:
+                stack.pop()
+                answer.pop()
+        
+        answer.reverse()  # remember to reverse it
+        return answer
