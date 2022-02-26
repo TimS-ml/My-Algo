e809c7aaae76f39cc3c0210042ad415088e64958
TimS <jianshen@pku.edu.cn>
2022-02-26 00:59:49 -0800

    314

--- LeetCode/314-m-0.py
+++ LeetCode/314-m-0.py
@@ -4,12 +4,17 @@
 - Space complexity: O()
 
 What do I need:
-<column, row> of each point
+<x, y> of each point
 
 BFS with row alignment
+Create a stack (?) tracking
+root.left  = <x_root.x - 1, x_root.y - 1>
+root.right = <x_root.x + 1, x_root.y - 1>
 '''
 
 from typing import List
+from collections import defaultdict
+
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -21,4 +26,17 @@ class TreeNode:
 
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        
+        columnTable = defaultdict(list)
+        queue = deque([(root, 0)])  # the <node, column> pattern
+
+        while queue:
+            node, column = queue.popleft()
+
+            if node is not None:
+                columnTable[column].append(node.val)
+
+                queue.append((node.left, column - 1))
+                queue.append((node.right, column + 1))
+
+        return [columnTable[x] for x in sorted(columnTable.keys())]
+
