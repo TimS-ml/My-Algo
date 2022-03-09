57c45da0ffc4487a9478ccbfc8d3a3a470a86c66
TimS <jianshen@pku.edu.cn>
2022-03-08 19:39:31 -0800

    560

--- LeetCode/560-0.py
+++ LeetCode/560-0.py
@@ -3,22 +3,12 @@
 - Time complexity: O()
 - Space complexity: O()
 
-input:  [3, 4,  7,  2, -3,  1,  4,  2,  1], K = 7
-sum: [0, 3, 7, 14, 16, 13, 14, 18, 20, 21]
-     |------|---------------|
-                |-----------------------|
+input:     [3, 4,  7,  2, -3,  1,  4,  2,  1], K = 7
+presum: [0, 3, 7, 14, 16, 13, 14, 18, 20, 21]
+        |------|---------------|
+                   |-----------------------|
 
-same as lc 001, create a dict
-- 
-- 
-
-# Pros and Cons:
-## Pros:
-
-## Cons:
-
-# Notation:
-
+same as lc 001, create a count dict on presum
 '''
 
 from typing import List
@@ -26,21 +16,22 @@ from typing import List
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        sumdict = {0: 1}
-        n = len(nums)
-        count = 0
-        s = 0
+        sum_dict = {0: 1}  # a count dict
+        ans = 0
+        presum = 0
 
         for num in nums:
-            print(num, count, sumdict)
-            s += num
-            if s - k in sumdict:
-                count += sumdict[s - k]
-            if s in sumdict:
-                sumdict[s] += 1  # why ?
-            else:
-                sumdict[s] = 1
-        return count
+            presum += num
+
+            if presum - k in sum_dict:
+                # from above example, if presum = 21, k = 7
+                # then the gap between 14(presum-k) and 21 is your target
+                # you only need to worry about how many times this 14 apprears
+                # attention: add 0 to sum_dict!!!
+                ans += sum_dict[presum - k]
+            
+            sum_dict[presum] = sum_dict.get(persum, 0) + 1
+        return ans
 
 
 nums = [3, 4, 7, 2, -3, 1, 4, 2, 1]
