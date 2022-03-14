5690166ff1dc9bfd10f8b0d222aa6aea29fc647f
TimS <jshen44@fordham.edu>
2022-03-14 15:54:07 -0700

    lol

--- LeetCode/227-0.py
+++ LeetCode/227-0.py
@@ -3,14 +3,19 @@
 - Time complexity: O()
 - Space complexity: O()
 
+- operator needs the number before (that is easy) and after
+    - sol: when we find the next operator, we do current calc
 '''
 
 class Solution:
     def calculate(self, s: str) -> int:
+        # sign is previous operator
         num, stack, sign = 0, [], "+"
         for i in range(len(s)):
             if s[i].isdigit():
                 num = num * 10 + int(s[i])
+
+            # edge case, last dig
             if s[i] in "+-*/" or i == len(s) - 1:
                 if sign == "+":
                     stack.append(num)
@@ -22,4 +27,5 @@ class Solution:
                     stack.append(int(stack.pop()/num))
                 num = 0
                 sign = s[i]
-        return sum(stack)
+
+        return sum(stack)  # since '-' is append as -num
