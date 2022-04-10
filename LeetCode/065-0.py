8d641589b7e405095158140579e7ed1e0615a797
TimS <jshen44@fordham.edu>
2022-04-10 00:42:44 -0700

    lol

---
+++ LeetCode/065-0.py
@@ -0,0 +1,29 @@
+'''
+# Code Explain:
+- Time complexity: O()
+- Space complexity: O()
+
+'''
+
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        seen_digit = seen_exponent = seen_dot =  False
+        for i, c in enumerate(s):
+            if c.isdigit():
+                seen_digit = True
+            elif c in ["+", "-"]:
+                if i > 0 and s[i - 1] != "e" and s[i - 1] != "E":
+                    return False
+            elif c in ["e", "E"]:
+                if seen_exponent or not seen_digit:
+                    return False
+                seen_exponent = True
+                seen_digit = False
+            elif c == ".":
+                if seen_dot or seen_exponent:
+                    return False
+                seen_dot = True
+            else:
+                return False
+        
+        return seen_digit
