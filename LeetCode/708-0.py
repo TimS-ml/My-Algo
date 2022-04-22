8d169e52da67cd54bdc8ba0c377e7fef4018429a
TimS <jshen44@fordham.edu>
2022-04-22 04:40:47 -0700

    lol

--- LeetCode/708-0.py
+++ LeetCode/708-0.py
@@ -21,14 +21,21 @@ class Solution:
         prev, curr = head, head.next
         
         while prev.next != head:
-            # Case1: 1 <- Node(2) <- 3
+            # Case1: 
+            # 1 <- Node(2) <- 3
+            # prev < curr
             if prev.val <= node.val <= curr.val:
                 break
             
-            # Case2: 3 -> 1, 3 -> Node(4) -> 1, 3 -> Node(0) -> 1
-            if prev.val > curr.val and (node.val > prev.val or node.val < curr.val):
+            # Case2 and 3: beginning and end of loop
+            # 3 -> 1, 3 -> Node(4) -> 1
+            if prev.val > curr.val and node.val > prev.val:
                 break
             
+            # 3 -> 1, 3 -> Node(0) -> 1
+            if prev.val > curr.val and node.val < curr.val:
+                break
+
             prev, curr = prev.next, curr.next
 
         # Insert node.
