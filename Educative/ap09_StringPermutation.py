eacd46946f36a7454a5412d9b6b410e34d4c6a65
TimS <jshen44@fordham.edu>
2022-06-19 01:07:12 -0700

    lol

--- Educative/ap09_StringPermutation.py
+++ Educative/ap09_StringPermutation.py
@@ -5,8 +5,37 @@
 
 '''
 
-def find_permutation(str1, pattern):
-    pass
+def find_permutation(arr, pattern):
+    freqArr, freqPattern = {}, {}
+    for c in pattern:
+        freqPattern[c] = freqPattern.get(c, 0) + 1
+
+    l, r = 0, 0
+    valid = 0
+    while r < len(arr):
+        c = arr[r]
+        r += 1
+
+        if c in freqPattern:
+            freqArr[c] = freqArr.get(c, 0) + 1
+
+            if freqArr[c] == freqPattern[c]:
+                valid += 1
+
+        if valid == len(freqPattern):
+            return True
+
+        if r >= len(pattern):
+            d = arr[l]
+            l += 1
+
+            if d in freqPattern:
+                if freqArr[d] == freqPattern[d]:
+                    valid -= 1
+                freqArr[d] -= 1
+
+    return valid == len(freqPattern)
+
 
 
 def main():
