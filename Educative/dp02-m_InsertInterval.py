38d5a2dfa7030aef45c16ef8c569972412259bfd
TimS <TimS-ml@users.noreply.github.com>
2022-06-26 17:32:25 -0700

    Educative

--- Educative/dp02-m_InsertInterval.py
+++ Educative/dp02-m_InsertInterval.py
@@ -22,13 +22,43 @@ Not sorted by start time?
 '''
 
 
+# Option A
 def insert(intervals, new_interval):
+    for i in range(len(intervals)):
+        if intervals[i][0] > new_interval[0]:
+            intervals.insert(i, new_interval)
+            break
 
+    # print(intervals)
+    ans = []
+    start = intervals[0][0]
+    end = intervals[0][1]
+
+    for i in range(1, len(intervals)):
+        # start - interval - end - interval
+        if end > intervals[i][0]:
+            start = min(intervals[i][0], start)
+            end = max(intervals[i][1], end)
+        else:
+            ans.append([start, end])
+            start = intervals[i][0]
+            end = intervals[i][1]
+
+    ans.append([start, end])
+    return ans
+
+            
 
 def main():
     print("Intervals after inserting the new interval: " +
           str(insert([[1, 3], [5, 7], [8, 12]], 
                      [4, 6])))
+    print("Intervals after inserting the new interval: " +
+          str(insert([[1, 3], [5, 7], [8, 12]], 
+                     [5, 6])))
+    print("Intervals after inserting the new interval: " +
+          str(insert([[1, 3], [5, 7], [8, 12]], 
+                     [6, 7])))
     # print("Intervals after inserting the new interval: " +
     #       str(insert([[1, 3], [5, 7], [8, 12]],
     #                  [4, 10])))
