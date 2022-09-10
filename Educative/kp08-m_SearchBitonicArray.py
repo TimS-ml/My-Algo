a7b513b20b521aa647a1bc07b6a1d2b86e31b844
TimS <jshen44@fordham.edu>
2022-09-10 13:25:27 -0700

    lol

--- Educative/kp08-m_SearchBitonicArray.py
+++ Educative/kp08-m_SearchBitonicArray.py
@@ -18,6 +18,44 @@ thus
 
 
 def search_bitonic_array(arr, key):
+    def find_max(arr):
+        start, end = 0, len(arr) - 1
+        while start < end:
+            mid = start + (end - start) // 2
+            if arr[mid] > arr[mid + 1]:
+                end = mid
+            else:
+                start = mid + 1
+    
+        # at the end of the while loop, 'start == end'
+        return start
+
+    def binary_search(arr, key, start, end):
+        while start <= end:
+            mid = int(start + (end - start) / 2)
+    
+            if key == arr[mid]:
+                return mid
+    
+            if arr[start] < arr[end]:  # ascending order
+                if key < arr[mid]:
+                    end = mid - 1
+                else:  # key > arr[mid]
+                    start = mid + 1
+            else:  # descending order
+                if key > arr[mid]:
+                    end = mid - 1
+                else:  # key < arr[mid]
+                    start = mid + 1
+    
+        return -1  # element is not found
+
+    maxIndex = find_max(arr)
+    keyIndex = binary_search(arr, key, 0, maxIndex)
+    if keyIndex != -1:
+        return keyIndex
+    else:
+        return binary_search(arr, key, maxIndex + 1, len(arr) - 1)
 
 
 def main():
