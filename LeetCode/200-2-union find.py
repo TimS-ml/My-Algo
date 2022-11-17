976710937d2cda69b31b19b5bb5c676908a885e2
TimS <jshen44@fordham.edu>
2022-11-17 02:03:06 -0800

    blind 75

--- LeetCode/200-2-union find.py
+++ LeetCode/200-2-union find.py
@@ -3,6 +3,12 @@
 - Time complexity: O()
 - Space complexity: O()
 
+Union find in 1d
+usually 
+
+for i in range(row):
+    for j in range(col):
+        index = i*col + j
 '''
 
 class Solution(object):
@@ -13,21 +19,22 @@ class Solution(object):
         """
         if len(grid) == 0: return 0
         row = len(grid); col = len(grid[0])
+
+        # init ans
         self.count = sum(grid[i][j]=='1' for i in range(row) for j in range(col))
-        parent = [i for i in range(row*col)]
+        parent = [i for i in range(row*col)]  # groupTag
         
         def find(x):
             if parent[x]!= x:
                 return find(parent[x])
             return parent[x]
         
-        def union(x,y):
-            xroot, yroot = find(x),find(y)
-            if xroot == yroot: return 
+        def union(x, y):
+            xroot, yroot = find(x), find(y)
+            if xroot == yroot:
+                return 
             parent[xroot] = yroot
             self.count -= 1
-        
-        
         
         for i in range(row):
             for j in range(col):
@@ -38,39 +45,9 @@ class Solution(object):
                     union(index, index+1)
                 if i < row-1 and grid[i+1][j] == '1':
                     union(index, index+col)
+
         return self.count
 
-
-class UnionFind:
-    def __init__(self, grid):
-        m, n = len(grid), len(grid[0])
-        self.count = 0
-        self.parent = [-1] * (m * n)
-        self.rank = [0] * (m * n)
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == "1":
-                    self.parent[i * n + j] = i * n + j
-                    self.count += 1
-
-    def find(self, i):
-        if self.parent[i] != i:
-            self.parent[i] = self.find(self.parent[i])
-        return self.parent[i]
-
-    def union(self, x, y):
-        rootx = self.find(x)
-        rooty = self.find(y)
-        if rootx != rooty:
-            if self.rank[rootx] < self.rank[rooty]:
-                rootx, rooty = rooty, rootx
-            self.parent[rooty] = rootx
-            if self.rank[rootx] == self.rank[rooty]:
-                self.rank[rootx] += 1
-            self.count -= 1
-
-    def getCount(self):
-        return self.count
 
 class Solution_2(object):
     def numIslands(self, grid):
@@ -85,9 +62,11 @@ class Solution_2(object):
             return 0
         m = len(grid)
         n = len(grid[0])
+
         # two dimension to one
         groupTag = [0 for i in range(m*n)]
 
+        # init groupTag val
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == '1':
@@ -118,6 +97,7 @@ class Solution_2(object):
         else:
             return self.find(groupTag[e], groupTag)
     
+    # in this example, x y is i j move 1
     def union(self, i, j, x, y, groupTag, n):
         index1 = i*n+j
         index2 = x*n+y
@@ -129,3 +109,44 @@ class Solution_2(object):
         else:
             groupTag[root2] = root1
 
+
+class UF:
+    def __init__(self, n):
+        self.p = [i for i in range(n)]
+        self.n = n
+        self.size = n  # init ans
+
+    def union(self, i, j):
+        pi, pj = self.find(i), self.find(j)
+        if pi != pj:
+            self.size -= 1
+            self.p[pj] = pi
+
+    def find(self, i):
+        if i != self.p[i]:
+            self.p[i] = self.find(self.p[i])
+        return self.p[i]
+
+
+class Solution_3:
+    def numIslands(self, grid):
+        if not grid:
+            return 0
+        m, n = len(grid), len(grid[0])
+        d = dict()
+        idx = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    d[i, j] = idx
+                    idx += 1
+
+        uf = UF(idx)
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    if i > 0 and grid[i-1][j] == '1':
+                        uf.union(d[i-1, j], d[i, j])
+                    if j > 0 and grid[i][j-1] == '1':
+                        uf.union(d[i, j-1], d[i, j])
+        return uf.size
--- LeetCode/200-Number of Islands.md
+++ LeetCode/200-Number of Islands.md
@@ -1,5 +1,6 @@
 # Links
 https://leetcode.com/problems/number-of-islands/
+https://leetcode.com/problems/number-of-islands/discuss/863366/Python-3-or-DFS-BFS-Union-Find-All-3-methods-or-Explanation
 
 # Thought Process
 
