423c9f1a12b3dcdb6c0aec27fd7506cf0f3a0305
TimS <jshen44@fordham.edu>
2023-01-03 23:58:32 -0800

    blind 75

--- LeetCode/371-0-bit.py
+++ LeetCode/371-0-bit.py
@@ -16,18 +16,39 @@ import pysnooper
 @pysnooper.snoop()
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        MAX_INT = 0x7FFFFFFF
+        MAX_INT = 0x7FFFFFFF  # bitmask of 31 1-bits
         MIN_INT = 0x80000000  # MAX_INT + 1
         MASK = 0x100000000  # 2^32
         while b:
             print(bin(a), bin(b))
-            # a, b = (a ^ b) % MASK, ((a & b) << 1) % MASK
-            carry = (a & b) << 1
-            a = (a ^ b) % MASK
-            b = carry % MASK
+            a, b = (a ^ b) % MASK, ((a & b) << 1) % MASK
+            # carry = (a & b) << 1
+            # a = (a ^ b) % MASK
+            # b = carry % MASK
+
         # if a is negative, get a's 32 bits complement positive first
         # then get 32-bit positive's Python complement negative
-        return a if a <= MAX_INT else ~((a % MIN_INT) ^ MAX_INT)
+        # return a if a <= MAX_INT else ~((a % MIN_INT) ^ MAX_INT)
+        return a if a < MAX_INT else ~(a ^ MASK)
+
+    # this is not good, it uses the multiplication
+    def getSum_2(self, a: int, b: int) -> int:
+        x, y = abs(a), abs(b)
+        # ensure x >= y
+        if x < y:
+            return self.getSum(b, a)  
+        sign = 1 if a > 0 else -1
+        
+        if a * b >= 0:
+            # sum of two positive integers
+            while y:
+                x, y = x ^ y, (x & y) << 1
+        else:
+            # difference of two positive integers
+            while y:
+                x, y = x ^ y, ((~x) & y) << 1
+        
+        return x * sign
 
 
 a = 1
