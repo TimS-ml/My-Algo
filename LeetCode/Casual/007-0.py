ecb3fb5bc8cec1ea45367e8beb3c67274728409e
TimS <jshen44@fordham.edu>
2023-01-10 02:45:11 -0800

    bit

--- LeetCode/Casual/007-0.py
+++ LeetCode/Casual/007-0.py
@@ -7,29 +7,44 @@
 Convert to string and reverse
 And integer should be within the integer range
 
-# Pros and Cons:
-## Pros:
-- No special cases needed to be concerned (for example 120)
-
-## Cons:
-
-# Notation:
 We can use:
     max_int = pow(2, 31) - 1
     min_int = pow(-2, 31)
 Instead of -2**31 and 2**31-1
 '''
 
+import math
+
 
 class Solution:
-    def reverse(self, x):
-        if x >= 0:
-            ans = int(str(x)[-1::-1])
-        else:
-            ans = -int(str(x)[-1:0:-1])
-        return ans if -2**31 < ans < 2**31 - 1 else 0
+    # def reverse(self, x):
+    #     if x >= 0:
+    #         ans = int(str(x)[-1::-1])
+    #     else:
+    #         ans = -int(str(x)[-1:0:-1])
+    #     return ans if -2**31 < ans < 2**31 - 1 else 0
 
-    def reverse2(self, x):
+    def reverse(self, x: int) -> int:
+        # Integer.MAX_VALUE = 2147483647 (end with 7)
+        # Integer.MIN_VALUE = -2147483648 (end with -8 )
+
+        MIN = -2147483648  # -2^31,
+        MAX = 2147483647  #  2^31 - 1
+
+        ans = 0
+        while x:
+            digit = int(math.fmod(x, 10))  # (python dumb) -1 %  10 = 9
+            x = int(x / 10)  # (python dumb) -1 // 10 = -1
+
+            if ans > MAX // 10 or (ans == MAX // 10 and digit > MAX % 10):
+                return 0
+            if ans < MIN // 10 or (ans == MIN // 10 and digit < MIN % 10):
+                return 0
+            ans = (ans * 10) + digit
+
+        return ans
+
+    def reverse_2(self, x):
         # sign = cmp(x, 0)  # only in python 2.x
         sign = -1 if x < 0 else 1
 
